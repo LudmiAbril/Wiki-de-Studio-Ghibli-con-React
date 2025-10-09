2dbd893774a62395b570bd8f43d6f27f15b6187e
LudmiAbril <ludmila.pereyra543@gmail.com>
2025-10-09 18:40:11 -0300

    resposnive home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,14 @@ import logo from "../assets/home-img.png";
 
 export function Home() {
   return (
-    <div className="fade-in flex items-center justify-center min-h-screen font-text">
-      <img src={logo} width={400} alt="logo" />
-      <div className="flex flex-col text-left">
+    <div className="fade-in flex flex-col lg:flex-row items-center justify-center min-h-screen font-text">
+      <img src={logo} alt="logo" className="w-full max-w-xs lg:max-w-md xl:max-w-lg mb-6 "
+      />
+      <div className="flex flex-col text-center items-center lg:text-left lg:items-start">
         <h1 className="font-title text-4xl mb-8">
           Welcome to Ghibli Wiki!
         </h1>
-        <p className="max-w-[60rem]">
+        <p className="px-6 lg:px-0 lg:max-w-[60rem]">
           This is a personal project created with React and Tailwind CSS, using the <a href="https://ghibliapi.vercel.app/" target="_blank" className="no-underline hover:underline text-[#4381cc]">Studio Ghibli API.</a> Here you can find information about the movies, characters, and other elements that make up the narrative universe of Ghibli.
         </p>
       </div>
