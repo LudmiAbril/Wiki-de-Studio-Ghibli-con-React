9d009af591ab1fdcc79d9fefaf0a7ec21308795e
LudmiAbril <ludmila.pereyra543@gmail.com>
2025-10-15 20:39:41 -0300

    film details responsive and styles fix

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -4,30 +4,49 @@ import { Link } from "react-router-dom";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import StarIcon from "@mui/icons-material/Star";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { loadingStyles } from "../utils/utils";
 
 export function FilmDetails() {
   const { id } = useParams();
-  const [film, setFilm] = useState([]);
+  const [film, setFilm] = useState({});
+  const [loading, setLoading] = useState(null);
 
   useEffect(() => {
     const fecthFilms = async () => {
       try {
+        setLoading(true)
         const response = await fetch(`https://ghibliapi.vercel.app/films/${id}`);
         const data = await response.json()
         setFilm(data)
       } catch (error) {
         console.log("error", error)
       }
+      finally {
+        setLoading(false)
+      }
     }
     fecthFilms();
   }, [id]);
 
+  const filmInfo = [
+    { label: "Original title", value: `"${film.original_title_romanised ?? ""}" (${film.original_title ?? ""})` },
+    { label: "Year", value: film.release_date ?? "" },
+    { label: "Director", value: film.director ?? "" },
+    { label: "Producer", value: film.producer ?? "" },
+    { label: "Running time", value: `${film.running_time ?? ""} min`, icon: <AccessTimeIcon className="text-sm" /> },
+    { label: "Score", value: film.rt_score ?? "", icon: <StarIcon className="text-sm" /> },
+  ];
+
+  if (loading) {
+    return <p className={loadingStyles}>Loading...</p>;
+  }
+
   return (
     <div
-      className="flex flex-col w-full min-h-screen overflow-hidden fade-in bg-cover bg-center bg-no-repeat"
+      className="flex flex-col w-full flex-grow overflow-hidden fade-in bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `
-      linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.95) 100%),
+      linear-gradient(to bottom, rgba(0, 0, 0, 0.27) 0%, rgba(0, 0, 0, 0.9) 100%),
       url(${film.movie_banner})
     `,
       }}
@@ -36,39 +55,32 @@ export function FilmDetails() {
         <p>Back</p>
       </Link>
 
-      <div className="flex flex-col flex-grow md:flex-row gap-16 justify-center items-center">
+      <div className="flex flex-col flex-grow lg:flex-row gap-16 justify-center items-center">
         <img
           src={film.image}
           alt={film.title + "image"}
-          className="w-80"
+          className="hidden lg:block lg:w-80"
         />
 
-        <div className="text-left">
-          <h3 className="font-title text-5xl mb-4 text-white drop-shadow-md shadow-black">
+        <div className="flex flex-col justify-center items-center text-center lg:text-left lg:items-start gap-1 p-6 lg:p-0">
+          <h3 className="font-title text-5xl mb-6 text-white drop-shadow-md shadow-black">
             {film.title}
           </h3>
-          <p className="font-text text-white italic text-left drop-shadow-md shadow-black">
-            Original title: "{film.original_title_romanised}" ({film.original_title})
-          </p>
-          <p className="font-text text-white italic text-left drop-shadow-md shadow-black">
-            Year: {film.release_date}
-          </p>
-          <p className="font-text text-white italic text-left drop-shadow-md shadow-black">
-            Director: {film.director}
-          </p>
-          <p className="font-text text-white italic text-left drop-shadow-md shadow-black">
-            Producer: {film.producer}
-          </p>
-          <p className="font-text text-white italic text-left drop-shadow-md shadow-black gap-1">
-            Running time: {film.running_time} min <AccessTimeIcon className="text-sm" />
-          </p>
-          <p className="font-text text-white italic text-left drop-shadow-md shadow-black flex items-center">
-            Score: {film.rt_score} <StarIcon className="text-sm" />
-          </p>
-          <p className="font-text text-white mt-8 text-left drop-shadow-md shadow-black max-w-[60rem]">
+
+          {filmInfo.map((info, index) => (
+            <p
+              key={index}
+              className={`font-text text-white italic drop-shadow-md shadow-black flex items-center gap-1`}
+            >
+              {info.label && `${info.label}:`} {info.value} {info.icon}
+            </p>
+          ))}
+
+          <p className="font-text text-white mt-8 drop-shadow-md shadow-black max-w-[22rem] lg:max-w-[60rem]">
             {film.description}
           </p>
         </div>
+
       </div>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="mt-auto text-center bg-slate-800 text-slate-400 italic p-2">
+        <div className="mt-auto text-center bg-black text-slate-400 italic p-2">
             <p>Ghibli Wiki - {new Date().getFullYear()}</p>
         </div>
     )
